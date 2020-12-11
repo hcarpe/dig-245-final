ed713fadcbfc9d5fa1f658b8c48816ae83e7608b
hcarpe <hcarpe@users.noreply.github.com>
2020-12-11 17:02:44 -0500

    trying to change image when I drag and drop

--- assets/js/main-ui.js
+++ assets/js/main-ui.js
@@ -8,6 +8,7 @@ var toolscomplete = false;
 var seedscomplete = false;
 var watercomplete = false;
 var dragZ = 10;
+var task = 0;
 
 function updateGame() {
   //handle logic here after dragging
@@ -20,6 +21,7 @@ function updateGame() {
  Add draggable / droppable functions
 ********************************************************/
 
+//tools icon dragging
 $(".toolsicon").draggable({
 	// always show dragging on top
 	drag: function(){
@@ -36,7 +38,7 @@ $(".toolsicon").draggable({
 			// disable dragging this
 			$(".toolsicon").draggable("disable");
 			toolscomplete = true;
-      $(".garden-area").attr("src", "assets/img/toolsicon.png");
+      $(".garden-area").attr("url","../img/cat.jpg");
 		} else {
 			console.log("wrong!", dropElem);
 			toolscomplete = false;
@@ -46,11 +48,74 @@ $(".toolsicon").draggable({
 	revertDuration: 200
 });
 
+//seeds icon dragging
+$(".seedsicon").draggable({
+	// always show dragging on top
+	drag: function(){
+		$(".seedsicon").css({
+			"z-index": ++dragZ
+		});
+	},
+	// if draggable was dropped on an accepting droppable elem
+	revert: function(dropElem) {
+    console.log("dropElem =", dropElem);
+		// if draggable was released over correct droppable
+		if (dropElem && dropElem.hasClass("garden-area")) {
+			console.log("correct!", dropElem);
+			// disable dragging this
+			$(".seedsicon").draggable("disable");
+			seedscomplete = true;
+      $(".garden-area").attr("src", "assets/img/seedsicon.png");
+		} else {
+			console.log("wrong!", dropElem);
+			seedscomplete = false;
+		}
+		updateGame();
+	},
+	revertDuration: 200
+});
+
+//watering can icon dragging
+$(".watercanicon").draggable({
+	// always show dragging on top
+	drag: function(){
+		$(".watercanicon").css({
+			"z-index": ++dragZ
+		});
+	},
+	// if draggable was dropped on an accepting droppable elem
+	revert: function(dropElem) {
+    console.log("dropElem =", dropElem);
+		// if draggable was released over correct droppable
+		if (dropElem && dropElem.hasClass("garden-area")) {
+			console.log("correct!", dropElem);
+			// disable dragging this
+			$(".watercanicon").draggable("disable");
+			watercomplete = true;
+      $(".garden-area").attr("src", "assets/img/seedsicon.png");
+		} else {
+			console.log("wrong!", dropElem);
+			watercomplete = false;
+		}
+		updateGame();
+	},
+	revertDuration: 200
+});
+
 
 // set which draggables the droppables accept
-$(".garden-area").droppable({
-	accept: ".toolsicon"
-});
-$(".gnome2-target").droppable({
-	accept: ".gnome2"
-});
+if (toolscomplete == false) {
+  $(".garden-area").droppable({
+  	accept: ".toolsicon"
+  });
+}
+if (toolscomplete == true && seedscomplete == false) {
+  $(".garden-area").droppable({
+  	accept: ".seedsicon"
+  });
+}
+if (toolscomplete == true && seedscomplete == true) {
+  $(".garden-area").droppable({
+  	accept: ".watercanicon"
+  });
+}
