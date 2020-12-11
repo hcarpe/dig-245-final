e8d2bed0eb83ce397a85a1f2229c14ff588bd18a
hcarpe <hcarpe@users.noreply.github.com>
2020-12-11 15:17:09 -0500

    Picture won't change

--- assets/js/main-ui.js
+++ assets/js/main-ui.js
@@ -6,10 +6,15 @@
 
 var toolscomplete = false;
 var seedscomplete = false;
-var watercomplate = false;
+var watercomplete = false;
 var dragZ = 10;
 
-
+function updateGame() {
+  //handle logic here after dragging
+	if (toolscomplete && seedscomplete && watercomplete){
+    console.log(4);
+  }
+}
 
 /*******************************************************
  Add draggable / droppable functions
@@ -28,13 +33,10 @@ $(".toolsicon").draggable({
 		// if draggable was released over correct droppable
 		if (dropElem && dropElem.hasClass("garden-area")) {
 			console.log("correct!", dropElem);
-			// update style
-			//$(".garden-area").css({
-				//"background-color": "green"
-			//});
 			// disable dragging this
 			$(".toolsicon").draggable("disable");
 			toolscomplete = true;
+      $(".garden-area").attr("src", "assets/img/toolsicon.png");
 		} else {
 			console.log("wrong!", dropElem);
 			toolscomplete = false;
@@ -45,101 +47,10 @@ $(".toolsicon").draggable({
 });
 
 
-
-$(".gnome2").draggable({
-	// always show dragging on top
-	drag: function(){
-		$(".gnome2").css({
-			"z-index": ++dragZ
-		});
-	},
-	// if draggable was dropped on an accepting droppable elem
-	revert: function(dropElem) {
-		// if draggable was released over correct droppable
-		if (dropElem && dropElem.hasClass("gnome2-target")) {
-			console.log("correct!", dropElem);
-			// update style
-			$(".gnome2-target").css({
-				"background-color": "green"
-			});
-			// disable dragging this
-			$(".gnome2").draggable("disable");
-			numberCorrect++;
-		} else {
-			console.log("wrong!", dropElem);
-			numberErrors++;
-		}
-		updateScore();
-	},
-	revertDuration: 200
-});
 // set which draggables the droppables accept
-$(".gnome1-target").droppable({
-	accept: ".gnome1"
+$(".garden-area").droppable({
+	accept: ".toolsicon"
 });
 $(".gnome2-target").droppable({
 	accept: ".gnome2"
 });
-
-/*******************************************************
- Single Page App functions
-********************************************************/
-
-// all others hidden on load
-gameStart();
-
-function gameStart() {
-	console.log("gameStart");
-	// hide / show sections
-	$(".game-start").show();
-	$(".game-play").hide();
-	$(".game-end").hide();
-}
-
-function gamePlay() {
-	// hide / show sections
-	$(".game-start").hide();
-	$(".game-play").show();
-	$(".game-end").hide();
-}
-
-function gameEnd() {
-	// hide / show sections
-	$(".game-start").hide();
-	$(".game-play").hide();
-	$(".game-end").show();
-}
-$(document).on("click", ".game-start-btn", gameStart);
-$(document).on("click", ".game-play-btn", gamePlay);
-$(document).on("click", ".game-end-btn", gameEnd);
-
-
-
-
-
-
-
-/*******************************************************
- Other functions
-********************************************************/
-
-function updateGame() {
-  //handle logic here after dragging
-	if (toolscomplete && seedscomplete && watercomplete){
-    console.log(4);
-  }
-}
-
-
-// play sound function
-//function playSound(selector) {
-	// get reference
-	//var audio = $(selector)[0];
-	// if muted (required for Chrome) then unmute
-	//if (audio.muted)
-		//audio.muted = false;
-	// if already playing then reset
-	//audio.currentTime = 0;
-	// finally, play
-	//audio.play();
-//}
