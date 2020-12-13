1e893d693f6cf312eddeee4e4a3c3e60e7e82f15
hcarpe <hcarpe@users.noreply.github.com>
2020-12-13 13:19:36 -0500

    binary file change

--- assets/js/main-ui.js
+++ assets/js/main-ui.js
@@ -8,45 +8,10 @@ var toolscomplete = false;
 var seedscomplete = false;
 var watercomplete = false;
 var dragZ = 10;
-var task = 0;
-
-function updateGame() {
-  //handle logic here after dragging
-	if (toolscomplete && seedscomplete && watercomplete){
-    console.log(4);
-  }
-}
 
 /*******************************************************
  Add draggable / droppable functions
 ********************************************************/
-
-//tools icon dragging
-$(".toolsicon").draggable({
-	// always show dragging on top
-	drag: function(){
-		$(".toolsicon").css({
-			"z-index": ++dragZ
-		});
-	},
-	// if draggable was dropped on an accepting droppable elem
-	revert: function(dropElem) {
-    console.log("dropElem =", dropElem);
-		// if draggable was released over correct droppable
-		if (dropElem && dropElem.hasClass("garden-area")) {
-			console.log("correct!", dropElem);
-			// disable dragging this
-			$(".toolsicon").draggable("disable");
-			toolscomplete = true;
-      $(".garden-area").attr(".garden-area","background-image: url(../img/cat.jpg);");
-		} else {
-			console.log("wrong!", dropElem);
-			toolscomplete = false;
-		}
-		updateGame();
-	},
-	revertDuration: 200
-});
 
 //seeds icon dragging
 $(".seedsicon").draggable({
@@ -65,12 +30,39 @@ $(".seedsicon").draggable({
 			// disable dragging this
 			$(".seedsicon").draggable("disable");
 			seedscomplete = true;
-      $(".garden-area").attr("src", "assets/img/seedsicon.png");
+      window.location.href = "../pages/seedspt2.html";
 		} else {
 			console.log("wrong!", dropElem);
 			seedscomplete = false;
+      window.location.href = "../pages/seedswrong.html";
 		}
-		updateGame();
+	},
+	revertDuration: 200
+});
+
+//tools icon dragging
+$(".toolsicon").draggable({
+	// always show dragging on top
+	drag: function(){
+		$(".toolsicon").css({
+			"z-index": ++dragZ
+		});
+	},
+	// if draggable was dropped on an accepting droppable elem
+	revert: function(dropElem) {
+    console.log("dropElem =", dropElem);
+		// if draggable was released over correct droppable
+		if (dropElem && dropElem.hasClass("garden-area")) {
+			console.log("correct!", dropElem);
+			// disable dragging this
+			$(".toolsicon").draggable("disable");
+			toolscomplete = true;
+      window.location.href = "../pages/toolspt2.html";
+		} else {
+			console.log("wrong!", dropElem);
+			toolscomplete = false;
+      window.location.href = "../pages/toolswrong.html";
+		}
 	},
 	revertDuration: 200
 });
@@ -92,30 +84,83 @@ $(".watercanicon").draggable({
 			// disable dragging this
 			$(".watercanicon").draggable("disable");
 			watercomplete = true;
-      $(".garden-area").attr("src", "assets/img/seedsicon.png");
+      window.location.href = "../pages/waterpt2.html";
 		} else {
 			console.log("wrong!", dropElem);
 			watercomplete = false;
+      window.location.href = "../pages/waterwrong.html";
 		}
-		updateGame();
+	},
+	revertDuration: 200
+});
+
+$(".trashicon").draggable({
+	// always show dragging on top
+	drag: function(){
+		$(".trashicon").css({
+			"z-index": ++dragZ
+		});
+	},
+	// if draggable was dropped on an accepting droppable elem
+	revert: function(dropElem) {
+    console.log("dropElem =", dropElem);
+		// if draggable was released over correct droppable
+		if (dropElem && dropElem.hasClass("garden-area")) {
+			console.log("correct!", dropElem);
+			// disable dragging this
+			$(".trashicon").draggable("disable");
+			watercomplete = true;
+      window.location.href = "../pages/trash.html";
+		} else {
+			console.log("wrong!", dropElem);
+		}
+	},
+	revertDuration: 200
+});
+
+$(".tractoricon").draggable({
+	// always show dragging on top
+	drag: function(){
+		$(".tractoricon").css({
+			"z-index": ++dragZ
+		});
+	},
+	// if draggable was dropped on an accepting droppable elem
+	revert: function(dropElem) {
+    console.log("dropElem =", dropElem);
+		// if draggable was released over correct droppable
+		if (dropElem && dropElem.hasClass("garden-area")) {
+			console.log("correct!", dropElem);
+			// disable dragging this
+			$(".tractoricon").draggable("disable");
+			watercomplete = true;
+      window.location.href = "../pages/tractor.html";
+		} else {
+			console.log("wrong!", dropElem);
+		}
 	},
 	revertDuration: 200
 });
 
 
 // set which draggables the droppables accept
-if (toolscomplete == false) {
-  $(".garden-area").droppable({
-  	accept: ".toolsicon"
-  });
-}
-if (toolscomplete == true && seedscomplete == false) {
+if (seedscomplete == false) {
   $(".garden-area").droppable({
   	accept: ".seedsicon"
+  });
+}
+if (seedscomplete == true && toolscomplete == false) {
+  $(".garden-area").droppable({
+  	accept: ".toolsicon"
   });
 }
 if (toolscomplete == true && seedscomplete == true) {
   $(".garden-area").droppable({
   	accept: ".watercanicon"
+  });
+}
+if (3==3) {
+  $(".garden-area").droppable({
+  	accept: ".wrongicon"
   });
 }
